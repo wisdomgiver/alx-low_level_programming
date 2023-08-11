6816d581bbb127004323b28ab7ea2ccfc8a5d241
Wisdom Obinna <obinnawisdom555@gmail.com>
2023-08-11 02:25:17 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,17 @@
-# include <stdio.h>
-/** main function
- * start
+/**
+ * File: 5-printf.c
+ * Desc: Program that prints a specific string using printf
  */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Outputs a given string to the console.
+ * Return: 0 upon successful completion.
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
