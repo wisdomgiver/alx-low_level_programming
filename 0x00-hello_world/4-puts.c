508e5821434c28509f47bcecf127bb3f59adc2e3
Wisdom Obinna <obinnawisdom555@gmail.com>
2023-08-11 01:19:28 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/*
+ *File: 4-puts.c
+ * Desc: Program that prints a specific string using puts.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Main entry point of the program.
+ * Description: Outputs a given string to the console.
+ * Return: 0 upon successful execution.
+ */
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
