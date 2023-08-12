fb5ad9f42994354d7d9146f117b919c8e2b01c2f
Wisdom Obinna <obinnawisdom555@gmail.com>
2023-08-11 19:16:38 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-        char letter;
+	char letter;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-                putchar(letter);
-        }
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
 
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
