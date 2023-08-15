90163f1bfadf69821e71e037e81ec871e472cf7f
Wisdom Obinna <obinnawisdom555@gmail.com>
2023-08-15 11:59:00 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point of the program
+ *
+ * Return:0
+ */
+void print_alphabet(void)
+{
+
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+}
