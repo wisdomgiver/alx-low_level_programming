2aac5b2d549b8c4c675825ae85a3d6586fae8762
Wisdom Obinna <obinnawisdom555@gmail.com>
2023-08-17 08:14:31 +0100

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Return:0 if successful
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
