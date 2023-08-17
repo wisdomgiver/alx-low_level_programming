ab916efd9dd1fa22522e48c5f8bd0c006dba74b7
Wisdom Obinna <obinnawisdom555@gmail.com>
2023-08-17 06:41:28 +0100

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point program
+ *
+ * Return:0 if successful
+ */
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	while (i < '9')
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+		i++;
+
+	}
+	_putchar('\n');
+}
