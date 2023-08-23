d66a92384a3967e4bd10249d10d9a9f386c07606
Wisdom Obinna <obinnawisdom555@gmail.com>
2023-08-23 18:49:19 +0100

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * _strncat - Entry point
+ * @dest: main string
+ * @src: second string to be concatenated
+ * @n: number of bytes from src
+ * Return: 0 if successful
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 	char *original_dest = dest;
 
-	while(*dest)
+	while (*dest)
 	{
 		dest++;
 	}
