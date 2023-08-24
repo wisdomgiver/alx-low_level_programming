f446e56562eb854f6d833254a191ab9d0d6b7b9b
Wisdom Obinna <obinnawisdom555@gmail.com>
2023-08-24 19:06:24 +0100

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+char *leet(char *str)
+{
+	int i, j;
+	char *original = str;
+
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for ( j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = numbers[j];
+				break;
+			}
+		}
+	}
+	return (original);
+}
