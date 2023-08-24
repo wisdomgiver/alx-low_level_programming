16a141f2777011bb315c099a56567bed0f829b06
Wisdom Obinna <obinnawisdom555@gmail.com>
2023-08-24 12:06:02 +0100

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
+/**
+ * reverse_array - Entry point
+ * @a: array of strings
+ * @n: the number of elements to swap
+ */
 void reverse_array(int *a, int n)
 {
-	int i = 0, temp; 
-	int j; 
-	j = n - 1;
-	while(i < j)
+	int i = 0, temp;
+	int j = n - 1;
+
+	while (i < j)
 	{
 		temp = a[i];
 		a[i] = a[j];
