2b96a4906e130a6a59d775fcc2d22e337e1ee4df
Wisdom Obinna <kcwisdom7@gmail.com>
2023-08-31 11:26:01 +0100

    task 0 and Readme file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
-/**
- * _puts_recursion - prints a string, followed by a new line
- * @s: string to print
- */
 void _puts_recursion(char *s)
 {
+
 	if (*s == '\0')
 	{
 		_putchar('\n');
