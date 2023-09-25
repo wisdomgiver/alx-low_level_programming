0f121525ad420aadae7a0a26f4d22f915a5bd0f8
Wisdom Obinna <kcwisdom7@gmail.com>
2023-09-25 17:39:20 +0100

    betty error fix for task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t nodes = 0;
+	size_t nodes = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        nodes++;
-        h = h->next;
-    }
+	while (h)
+	{
+		printf("%d\n", h->n);
+		nodes++;
+		h = h->next;
+	}
 
-    return nodes;
+	return (nodes);
 }
